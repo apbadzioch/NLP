30283f0de2fc9cef335f2a92b928056c039f568a
apbadzioch <abadzioch@gmail.com>
2025-10-22 16:12:04 -0500

    work on api and fix issues with pulling to another computer

--- codcad/openai_api.py
+++ codcad/openai_api.py
@@ -84,4 +84,18 @@ user_content1 = f'''I want to create a recipe blog post. Here are my dietary res
 {user_profile['dietary_restrictions']}. My cuisine preferences are: {user_profile['cuisine_preferences']}.
 The ingredients I have available are: {user_profile['ingredients_available']}.'''
 
+user_content2 = "Please provide a blog post with a title description, ingredients, and instructions. Format the ingredients and instructions as follows: Ingredients should be bulleted and instructions should be numbered."
 
+user_content3 = "The recipe must use only the listed ingredients and should result ina single blog post with instructions not exceeding six steps."
+
+user_prompt = {
+    "role": "user",
+    "content": user_content1 + "\n" + user_content2 + "\n" + user_content3
+}
+
+response = client.chat.completion.create(
+    model = "gpt-3.5-turbo",
+    messages = [system_prompt, user_prompt]
+)
+
+print(response.choices[0].message.content)
